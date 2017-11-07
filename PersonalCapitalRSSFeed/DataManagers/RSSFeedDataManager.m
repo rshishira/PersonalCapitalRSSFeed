e1bd9bbc9febc8c68cd1e72cca215714c4786e3c
Shishira Ramesh <shishira.ramesh@gmail.com>
2017-11-07 17:52:53 -0500

    Added Localized strings

--- PersonalCapitalRSSFeed/DataManagers/RSSFeedDataManager.m
+++ PersonalCapitalRSSFeed/DataManagers/RSSFeedDataManager.m
@@ -23,31 +23,30 @@
     
     NSURLSession *session = [NSURLSession sharedSession];
 
-    NSString *articleURL = @"https://blog.personalcapital.com/feed/?cat=3,891,890,68,284";
+    NSString *articleURL = NSLocalizedString(@"URL", nil);
     __weak typeof(self) weakSelf = self;
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:articleURL]
-                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-                                                if(error){
-                                                    completionHandler(nil, error);
-                                                    return;
-                                                }
-                                                weakSelf.articleArray = [[NSMutableArray alloc] init];
-                                                if(data == nil){
-                                                    completionHandler(nil, [NSError errorWithDomain:@"PersonalCapitalRSSFeed" code:-1 userInfo:@{NSLocalizedDescriptionKey:@"No data returned!"}]);
-                                                   return;
-                                                }
-                                                NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
-                                                [parser setDelegate:self];
-                                                
-                                                BOOL result = [parser parse];
-                                                if(result){
-                                                    completionHandler(weakSelf.articleArray, nil);
-                                                } else {
-                                                    completionHandler(nil, [NSError errorWithDomain:@"PersonalCapitalRSSFeed" code:-1 userInfo:@{NSLocalizedDescriptionKey:@"Error Parsing data"}]);
-                                                    return;
-                                                }
-                                                
-                                            }];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:articleURL] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            if(error){
+                completionHandler(nil, error);
+                return;
+            }
+            weakSelf.articleArray = [[NSMutableArray alloc] init];
+            if(data == nil){
+                completionHandler(nil, [NSError errorWithDomain:NSLocalizedString(@"PersonalCapitalRSSFeed", nil) code:-1 userInfo:@{NSLocalizedDescriptionKey:NSLocalizedString(@"NoDataMessage", nil)}]);
+               return;
+            }
+            NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
+            [parser setDelegate:self];
+        
+            BOOL result = [parser parse];
+            if(result){
+                completionHandler(weakSelf.articleArray, nil);
+            } else {
+                completionHandler(nil, [NSError errorWithDomain:NSLocalizedString(@"PersonalCapitalRSSFeed", nil) code:-1 userInfo:@{NSLocalizedDescriptionKey:NSLocalizedString(@"ParsingErrorMessage", nil)}]);  
+                return;
+            }
+        
+        }];
     [dataTask resume];
 }
 
@@ -71,7 +70,8 @@
     if ([self.currentElementName isEqualToString:@"title"]){
         self.currentArticle.title = self.currentString;
     } else if ([self.currentElementName isEqualToString:@"link"]){
-        NSString *appendedString = [self.currentString stringByAppendingString:@"?displayMobileNavigation=0"];
+        //Append this string to eliminate navigationbar contained within the rendered website.
+        NSString *appendedString = [self.currentString stringByAppendingString:NSLocalizedString(@"displayMobileNavigation", nil)];
         self.currentArticle.link = appendedString;
     } else if ([self.currentElementName isEqualToString:@"pubDate"]){
         self.currentArticle.publishDate = self.currentString;
